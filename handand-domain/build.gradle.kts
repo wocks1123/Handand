f2b9f8fc16974dd573930087071a2c62c309f977
wocks1123 <wocks1123@gmail.com>
2025-06-07 02:33:20 +0900

    chore: validation 의존성 추가

--- handand-domain/build.gradle.kts
+++ handand-domain/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("jakarta.validation:jakarta.validation-api:3.1.1")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("io.jsonwebtoken:jjwt:0.12.5")
 
